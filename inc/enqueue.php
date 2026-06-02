223d97fe064c41fe284b4e44349c852a46c8463b
Pashtet221 <90337059+Pashtet221@users.noreply.github.com>
2026-06-02 21:26:24 +0300

    Apply buy button setting color inline

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -17,8 +17,11 @@ function gelikon_enqueue_assets() {
 add_action('wp_enqueue_scripts', 'gelikon_enqueue_assets');
 
 function gelikon_inline_css_variables() {
+    $buy_button_color = sanitize_hex_color(get_theme_mod('gelikon_color_buy_button', '#12D457'));
+    $buy_button_color = $buy_button_color ?: '#12D457';
+
     $vars = [
-        '--gl-color-buy-button' => sanitize_hex_color(get_theme_mod('gelikon_color_buy_button', '#12D457')),
+        '--gl-color-buy-button' => $buy_button_color,
         '--gl-color-accent'     => sanitize_hex_color(get_theme_mod('gelikon_color_accent', '#12D457')),
         '--gl-color-accent-2'   => sanitize_hex_color(get_theme_mod('gelikon_color_accent_secondary', '#1ea751')),
         '--gl-color-menu'       => sanitize_hex_color(get_theme_mod('gelikon_color_menu', '#9CA3AF')),
@@ -31,6 +34,39 @@ function gelikon_inline_css_variables() {
         }
     }
     $css .= '}';
+
+    $buy_button_selectors = [
+        '.gl-product-card .button:not(.gl-product-card__button--disabled)',
+        '.gl-product-card a.button:not(.gl-product-card__button--disabled)',
+        '.gl-product-card__button:not(.gl-product-card__button--disabled)',
+        'a.gl-product-card__button:not(.gl-product-card__button--disabled)',
+        '.gl-product-mobile-bar__button .single_add_to_cart_button',
+        '.gl-product-mobile-bar__button button.single_add_to_cart_button.button.alt',
+        '.gl-product-desktop-bar__right .single_add_to_cart_button',
+        '.gl-product-desktop-bar__right button.single_add_to_cart_button.button.alt',
+        '.gl-product-buybox .single_add_to_cart_button',
+        '.gl-product-buybox button.single_add_to_cart_button.button.alt',
+        '.single_add_to_cart_button.is-in-cart',
+    ];
+
+    $buy_button_hover_selectors = [
+        '.gl-product-card .button:not(.gl-product-card__button--disabled):hover',
+        '.gl-product-card a.button:not(.gl-product-card__button--disabled):hover',
+        '.gl-product-card__button:not(.gl-product-card__button--disabled):hover',
+        'a.gl-product-card__button:not(.gl-product-card__button--disabled):hover',
+        '.gl-product-mobile-bar__button .single_add_to_cart_button:hover',
+        '.gl-product-mobile-bar__button button.single_add_to_cart_button.button.alt:hover',
+        '.gl-product-desktop-bar__right .single_add_to_cart_button:hover',
+        '.gl-product-desktop-bar__right button.single_add_to_cart_button.button.alt:hover',
+        '.gl-product-buybox .single_add_to_cart_button:hover',
+        '.gl-product-buybox button.single_add_to_cart_button.button.alt:hover',
+    ];
+
+    $buy_button_css = 'background:' . $buy_button_color . ' !important;';
+    $buy_button_css .= 'border-color:' . $buy_button_color . ' !important;';
+
+    $css .= implode(',', $buy_button_selectors) . '{' . $buy_button_css . '}';
+    $css .= implode(',', $buy_button_hover_selectors) . '{' . $buy_button_css . '}';
 
     wp_add_inline_style('gelikon-main', $css);
 }
