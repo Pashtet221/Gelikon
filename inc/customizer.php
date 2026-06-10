321cd49d1975094e074d2a76c2c4b6aeb7107008
Pashtet221 <90337059+Pashtet221@users.noreply.github.com>
2026-06-10 23:29:44 +0300

    Fix checkout and consent UI

--- inc/customizer.php
+++ inc/customizer.php
@@ -63,5 +63,45 @@ function gelikon_customize_register($wp_customize) {
             'type'    => 'text',
         ]);
     }
+
+
+    $wp_customize->add_section('gelikon_legal_texts', [
+        'title'       => __('Юридические тексты', 'gelikon'),
+        'panel'       => 'gelikon_theme_options',
+        'priority'    => 40,
+        'description' => __('Тексты согласий выводятся в чекбоксах форм сайта. Можно вставлять HTML-ссылки на нужные документы.', 'gelikon'),
+    ]);
+
+    $wp_customize->add_setting('gelikon_personal_data_consent_text', [
+        'default'           => function_exists('gelikon_personal_data_consent_default_text') ? gelikon_personal_data_consent_default_text() : '',
+        'sanitize_callback' => 'gelikon_sanitize_legal_text',
+        'transport'         => 'refresh',
+    ]);
+
+    $wp_customize->add_control('gelikon_personal_data_consent_text', [
+        'label'       => __('Текст чекбокса согласия', 'gelikon'),
+        'description' => __('Например: Я ознакомлен(а) с <a href="/privacy-policy/">Политикой...</a> и принимаю условия <a href="/public-offer/">Публичной оферты</a>.', 'gelikon'),
+        'section'     => 'gelikon_legal_texts',
+        'type'        => 'textarea',
+    ]);
 }
 add_action('customize_register', 'gelikon_customize_register');
+
+function gelikon_sanitize_legal_text($value) {
+    return wp_kses($value, [
+        'a'      => [
+            'href'   => true,
+            'target' => true,
+            'rel'    => true,
+            'title'  => true,
+        ],
+        'br'     => [],
+        'strong' => [],
+        'b'      => [],
+        'em'     => [],
+        'i'      => [],
+        'span'   => [
+            'class' => true,
+        ],
+    ]);
+}
