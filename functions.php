2e3e7beb09fab906ece51bd5ea5359165b7c9b14
Pashtet221 <90337059+Pashtet221@users.noreply.github.com>
2026-06-21 15:55:36 +0300

    21
    21

--- functions.php
+++ functions.php
@@ -7805,61 +7805,12 @@ add_action('admin_init', function () {
 
 
 
+function gelikon_category_path($value) {
+    $items = array_filter(array_map('trim', explode(',', $value)));
 
+    $items = array_filter($items, function($item) {
+        return mb_strtolower($item) !== 'главная';
+    });
 
-
-
-
-
-// add_action('pmxi_saved_post', 'gl_import_external_images_to_media', 10, 3);
-
-// function gl_import_external_images_to_media($post_id, $xml_node, $is_update) {
-//     $post = get_post($post_id);
-
-//     if (!$post || $post->post_type !== 'post') {
-//         return;
-//     }
-
-//     $content = $post->post_content;
-
-//     if (strpos($content, '<img') === false) {
-//         return;
-//     }
-
-//     preg_match_all('/<img[^>]+src=["\']([^"\']+)["\']/i', $content, $matches);
-
-//     if (empty($matches[1])) {
-//         return;
-//     }
-
-//     require_once ABSPATH . 'wp-admin/includes/media.php';
-//     require_once ABSPATH . 'wp-admin/includes/file.php';
-//     require_once ABSPATH . 'wp-admin/includes/image.php';
-
-//     foreach ($matches[1] as $old_url) {
-//         if (strpos($old_url, '/image/catalog/') === false) {
-//             continue;
-//         }
-
-//         $attachment_id = media_sideload_image($old_url, $post_id, null, 'id');
-
-//         if (is_wp_error($attachment_id)) {
-//             continue;
-//         }
-
-//         $new_url = wp_get_attachment_url($attachment_id);
-
-//         if ($new_url) {
-//             $content = str_replace($old_url, $new_url, $content);
-//         }
-//     }
-
-//     remove_action('pmxi_saved_post', 'gl_import_external_images_to_media', 10);
-
-//     wp_update_post([
-//         'ID'           => $post_id,
-//         'post_content' => $content,
-//     ]);
-
-//     add_action('pmxi_saved_post', 'gl_import_external_images_to_media', 10, 3);
-// }
+    return implode(' > ', $items);
+}
