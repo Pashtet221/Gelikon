b88a161018aac63a093a98fe1497a2ddd0761b83
Pashtet221 <90337059+Pashtet221@users.noreply.github.com>
2026-07-02 23:22:26 +0300

    Add category tabs to blog index

--- index.php
+++ index.php
@@ -2,14 +2,38 @@
 <main id="primary" class="site-main">
     <div class="gl-container gl-page">
         <?php if (have_posts()) : ?>
-            <div class="gl-posts-grid">
+            <?php
+            $post_category_tabs = get_categories([
+                'hide_empty' => true,
+                'orderby'    => 'name',
+                'order'      => 'ASC',
+            ]);
+            ?>
+            <?php if (!empty($post_category_tabs)) : ?>
+                <div class="gl-post-category-tabs" data-gl-post-category-tabs>
+                    <div class="gl-post-category-tabs__list" role="tablist" aria-label="<?php esc_attr_e('Рубрики записей', 'gelikon'); ?>">
+                        <button class="gl-post-category-tabs__button is-active" type="button" role="tab" aria-selected="true" data-gl-post-category-tab="all">
+                            <?php esc_html_e('Все', 'gelikon'); ?>
+                        </button>
+                        <?php foreach ($post_category_tabs as $post_category_tab) : ?>
+                            <button class="gl-post-category-tabs__button" type="button" role="tab" aria-selected="false" data-gl-post-category-tab="<?php echo esc_attr($post_category_tab->slug); ?>">
+                                <?php echo esc_html($post_category_tab->name); ?>
+                            </button>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <div class="gl-posts-grid" data-gl-posts-grid>
                 <?php while (have_posts()) : the_post(); ?>
-                    <article <?php post_class('gl-card gl-post-card'); ?>>
+                    <?php
+                    $post_categories = get_the_category();
+                    $post_category_slugs = wp_list_pluck($post_categories, 'slug');
+                    ?>
+                    <article <?php post_class('gl-card gl-post-card'); ?> data-gl-post-categories="<?php echo esc_attr(implode(' ', $post_category_slugs)); ?>">
                         <?php if (has_post_thumbnail()) : ?>
                             <a class="gl-post-card__thumb" href="<?php the_permalink(); ?>"><?php the_post_thumbnail('gelikon-card'); ?></a>
                         <?php endif; ?>
                         <div class="gl-post-card__content">
-                            <?php $post_categories = get_the_category(); ?>
                             <?php if (!empty($post_categories)) : ?>
                                 <div class="gl-post-card__categories">
                                     <?php foreach ($post_categories as $post_category) : ?>
